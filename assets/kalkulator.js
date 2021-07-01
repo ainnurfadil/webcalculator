131e262af0eaec698bfea783f3c73e8c0d9cde60
ainnurfadil <fadilainnur@gmail.com>
2021-07-01 16:08:25 +0700

    Update kalkulator.js

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,1 +1,40 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+};
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+    button.addEventListener('click', function(event) {
+
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay()
+    });
+}
+
+function inputDigit(digit) {
+    if(calculator.displayNumber === '0') {
+        calculator.displayNumber = digit;
+    }else{
+        calculator.displayNumber += digit;
+    }
+}
