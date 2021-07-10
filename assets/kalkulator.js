4b89916a8a3ca4da3e1e7e985f8c1c9f49252870
ainnurfadil <fadilainnur@gmail.com>
2021-07-11 02:23:19 +0700

    final training js basic

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -20,11 +20,69 @@ function inputDigit(digit) {
     calculator.displayNumber += digit;
 }
 
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator has been applied')
+    }
+}
+
+function performCalculator() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("you not set yet the operator");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+    }
+
+    calculator.displayNumber = result;
+}
+
 const buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
     button.addEventListener('click', function(event) {
 
         const target = event.target;
+
+        if(target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+        
+        if(target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('equals')) {
+            performCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('operator')) {
+            handleOperator(target.innerText);
+            return;
+        }
 
         inputDigit(target.innerText);
         updateDisplay()
